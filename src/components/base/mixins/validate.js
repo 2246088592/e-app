b477c1ea4c81431bc499aed19708645628363d2e
cjl <2553520536@qq.com>
2019-06-24 13:26:01 +0800

    更改事件发射时机

--- src/components/base/mixins/validate.js
+++ src/components/base/mixins/validate.js
@@ -1,3 +1,5 @@
+let app = getApp()
+
 export default {
   methods: {
     // 校验方法
@@ -13,6 +15,9 @@ export default {
         if (value) {
           result = this.props.onValidate(value) ? 'success' : 'error'
         }
+      }
+      if (this.props.model.fid) {
+        app.emitter.emit(`${this.props.model.fid}`, Object.assign({ ...this.props.model }, { status: result }))
       }
       if (this.props.model.status === result) {
         return
